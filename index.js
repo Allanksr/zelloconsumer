0fc25e85b70346ddeaee194be69076d515b2453e
Allan Ribas <allanksr@gmail.com>
2020-11-08 10:46:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const zellosdk = require('./sdk/latest-zcc.sdk.js')
-
-TokenManager = require('./tk')
-
-function zelloconsumer(options){
-    console.log('issuer', options.issuer)
-    console.log('privatekey', options.privatekey)
-    var token = TokenManager.createJwt(`${kissuer}`, privateKey)
-
-    return token
-}
-
-
+const fs = require('fs')
+ TokenManager = require('./tk')
+    function zelloconsumer(args0){
+        var issuer = args0['issuer']
+        var private = args0['private']
+        return new Promise( (resolve, reject) => {
+            fs.readFile(issuer,  (err, issuer) =>{
+                if(!err){
+                    fs.readFile(private,  (err, private) =>{
+                        if(!err){
+                            var token = TokenManager.createJwt(`${issuer}`, private)
+                            resolve({response: token})
+                        }else{
+                            reject({error: err})
+                        }                  
+                    })
+                }else{
+                    reject({error: err})
+                }          
+        })      
+        }).then(token =>  token )
+    }
 module.exports.zelloconsumer = zelloconsumer
